daad34292cb1f8cce92640adbae4107ca83e08ed
Innocent Anozie <iygeal@gmail.com>
2023-11-10 23:05:01 +0100

    Recreated _setenv.c

--- _setenv.c
+++ _setenv.c
@@ -1,53 +1,21 @@
 #include "shell.h"
 /**
- * _setenv - Function that add or modifies environment variables
- * @name: Name of the variable
- * @value: The value to set it the variable to
- * @overwrite: Flag to determine whether to overwrite if name already exists
- * Return: 0 on success; -1 on failure
+ * _setenv - Function that add/modifies an environment variable
+ * @name: The environment variable
+ * @value: Value to set the variable to
+ * Return: 0
  */
-int _setenv(const char *name, const char *value, int overwrite)
+int _setenv(char *name, const char *value)
 {
-	int newEnvSize;
-	char **envPtr = environ, **newEnvArray;
-
-	if (name == NULL || value == NULL || _strchr(name, '=') != NULL)
-		return (-1);
-	if (name[0] == '\0' || value[0] == '\0')
-		return (-1);
-	while (*envPtr != NULL)
+	if (name == NULL || value == NULL)
 	{
-		if (_strncmp(*envPtr, name, _strlen(name)) == 0)
-			break;
-		envPtr++;
+		fprintf(stderr, "setenv: Variable not found\n");
+		return (-1);
 	}
-	if (*envPtr != NULL)
+	if (setenv(name, value, 1) != 0)
 	{
-		if (overwrite)
-		{
-			free(*envPtr);
-			*envPtr = malloc(_strlen(name) + _strlen(value) + 2);
-			if (*envPtr == NULL)
-				return (-1);
-			sprintf(*envPtr, "%s=%s", name, value);
-		}
-	} else
-	{
-		newEnvSize = (envPtr - environ) + 2;
-		newEnvArray = malloc(newEnvSize * sizeof(char *));
-		if (newEnvArray == NULL)
-			return (-1);
-		_memcpy(newEnvArray, environ, (envPtr - environ) * sizeof(char *));
-		newEnvArray[newEnvSize - 2] = malloc(_strlen(name) + _strlen(value) + 2);
-		if (newEnvArray[newEnvSize - 2] == NULL)
-		{
-			free(newEnvArray);
-			return (-1);
-		}
-		sprintf(newEnvArray[newEnvSize - 2], "%s=%s", name, value);
-		newEnvArray[newEnvSize - 1] = NULL;
-		free(environ);
-		environ = newEnvArray;
+		perror("setenv");
+		return (-1);
 	}
 	return (0);
 }
