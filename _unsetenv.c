8e40aee2f819106c754810bff010fdecad5e9146
Innocent Anozie <iygeal@gmail.com>
2023-11-10 01:43:10 +0100

    Created _unsetenv

---
+++ _unsetenv.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+/**
+ * _unsetenv - Function that unsets environment variable
+ * @name: The variable name
+ * Return: 0
+ */
+int _unsetenv(const char *name)
+{
+	if (!name)
+	{
+		fprintf(stderr, "unsetenv: Variable not found\n");
+		return (-1);
+	}
+	if (unsetenv(name) != 0)
+	{
+		perror("unsetenv now");
+		return (-1);
+	}
+	return (0);
+}
